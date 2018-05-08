86cab960d4572000201e486582154ea5a0a4f93f
xpulen <pulen.gabor@gmail.com>
2018-05-08 14:09:33 +0200

    Prvy commit, test

---
+++ registration.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="registration.css">
+    <title>
+        Projekt ku skúške
+    </title>
+
+</head>
+<body>
+<header>
+    <h1>
+        Názov stránky
+    </h1>
+</header>
+<div>
+
+</div>
+<!--<footer>-->
+<!--    <h2>-->
+<!--        *footer here*-->
+<!--    </h2>-->
+<!--</footer>-->
+
+</body>
+</html>
